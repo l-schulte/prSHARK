342f0904dd2bc21079a0577519f1e7887f7ac309
To Que Phuong Tran <tran16@ads.uni-passau.de>
2026-06-30 15:02:38 +0200

    Align current code with pycoSHARK 2.0.0

--- plugin_packaging/execute.sh
+++ plugin_packaging/execute.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PLUGIN_PATH=${1}
 
-COMMAND="python3.5 ${1}/smartshark_plugin.py --db-database ${4} --db-hostname ${5} --db-port ${6} --project-name ${8} --prurl ${9} --backend ${10}"
+COMMAND="python3 ${1}/smartshark_plugin.py --db-database ${4} --db-hostname ${5} --db-port ${6} --project-name ${8} --prurl ${9} --backend ${10}"
 
 if [ ! -z ${11} ] && [ ${11} != "None" ]; then
     COMMAND="$COMMAND --token ${11}"
